6ca05401b84184279d875602391dd8f6c09d9689
Max Eckard <eckardm@umich.edu>
2015-09-01 19:44:57 -0400

    fixed the echo and added yame

--- full-workout.bat
+++ full-workout.bat
@@ -1,7 +1,9 @@
-echo "Hajime!"
+@echo "Hajime!"
 
 @echo off
 "C:\Program Files\VideoLAN\VLC\vlc.exe" "C:\Users\eckardm\Videos\kata - estrada\VIDEO_TS\VTS_01_1.VOB" --play-and-exit
 "C:\Program Files\VideoLAN\VLC\vlc.exe" "C:\Users\eckardm\Videos\kata - estrada\VIDEO_TS\VTS_01_2.VOB" --play-and-exit
 "C:\Program Files\VideoLAN\VLC\vlc.exe" "C:\Users\eckardm\Videos\kata - estrada\VIDEO_TS\VTS_01_3.VOB" --play-and-exit
 "C:\Program Files\VideoLAN\VLC\vlc.exe" "C:\Users\eckardm\Videos\kata - estrada\VIDEO_TS\VTS_01_4.VOB" --play-and-exit
+
+@echo "Yame!"
