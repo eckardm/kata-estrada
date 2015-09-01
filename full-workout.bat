de3fb05ebab167643ff742533a563bf6b5e9c619
Max Eckard <eckardm@umich.edu>
2015-09-01 19:42:59 -0400

    fixed fourth video and added hajime!

--- full-workout.bat
+++ full-workout.bat
@@ -1,4 +1,7 @@
+echo "Hajime!"
+
+@echo off
 "C:\Program Files\VideoLAN\VLC\vlc.exe" "C:\Users\eckardm\Videos\kata - estrada\VIDEO_TS\VTS_01_1.VOB" --play-and-exit
 "C:\Program Files\VideoLAN\VLC\vlc.exe" "C:\Users\eckardm\Videos\kata - estrada\VIDEO_TS\VTS_01_2.VOB" --play-and-exit
 "C:\Program Files\VideoLAN\VLC\vlc.exe" "C:\Users\eckardm\Videos\kata - estrada\VIDEO_TS\VTS_01_3.VOB" --play-and-exit
-"C:\Program Files\VideoLAN\VLC\vlc.exe" "C:\Users\eckardm\Videos\kata - estrada\VIDEO_TS\VTS_01_1.VOB" --play-and-exit
+"C:\Program Files\VideoLAN\VLC\vlc.exe" "C:\Users\eckardm\Videos\kata - estrada\VIDEO_TS\VTS_01_4.VOB" --play-and-exit
