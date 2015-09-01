98340f4e50a24ba62b5c6388aae5669314986be7
Max Eckard <eckardm@umich.edu>
2015-09-01 19:28:44 -0400

    added full workout

---
+++ full-workout.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\VideoLAN\VLC\vlc.exe" "C:\Users\eckardm\Videos\kata - estrada\VIDEO_TS\VTS_01_1.VOB" --play-and-exit
+"C:\Program Files\VideoLAN\VLC\vlc.exe" "C:\Users\eckardm\Videos\kata - estrada\VIDEO_TS\VTS_01_2.VOB" --play-and-exit
+"C:\Program Files\VideoLAN\VLC\vlc.exe" "C:\Users\eckardm\Videos\kata - estrada\VIDEO_TS\VTS_01_3.VOB" --play-and-exit
+"C:\Program Files\VideoLAN\VLC\vlc.exe" "C:\Users\eckardm\Videos\kata - estrada\VIDEO_TS\VTS_01_1.VOB" --play-and-exit
